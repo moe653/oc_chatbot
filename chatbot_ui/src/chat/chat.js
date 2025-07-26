27c822e483aa674c1ce313bdb795594db2343487
moe653 <september11.m.i@icloud.com>
2025-07-26 20:53:25 +0900

    ポスターをチャット画面に追加

--- chatbot_ui/src/chat/chat.js
+++ chatbot_ui/src/chat/chat.js
@@ -153,6 +153,45 @@ const Chat = ({ theme }) => {
                     残り {"\n"}{MAX_LENGTH - inputText.length} 文字
                 </div>
             </div>
+
+            {/* ポスター */}
+            <div className={styles.posterArea}>
+                {theme === 'green' && (
+                    <img
+                        src="/fig/poster_security.png"
+                        alt="ポスター"
+                        className={styles.posterImage}
+                    />
+                )}
+                {theme === 'blue' && (
+                    <>
+                        <img
+                            src="/fig/poster_medical_chatbot.png"
+                            alt="医療ポスター1"
+                            className={styles.posterImage}
+                        />
+                        <img
+                            src="/fig/poster_medical_kokkaku.png"
+                            alt="医療ポスター2"
+                            className={styles.posterImage}
+                        />
+                    </>
+                )}
+                {theme === 'orange' && (
+                    <>
+                        <img
+                            src="/fig/poster_communication_2024.png"
+                            alt="コミュニケーションポスター2024"
+                            className={styles.posterImage}
+                        />
+                        <img
+                            src="/fig/poster_communication_2023.png"
+                            alt="コミュニケーションポスター2023"
+                            className={styles.posterImage}
+                        />
+                    </>
+                )}
+            </div>
         </div>
     );
 };
